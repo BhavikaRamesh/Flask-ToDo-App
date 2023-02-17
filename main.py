5315a4ed82ff9509390660663bd1764099416917
BhavikaRamesh <19wh1a0425@bvrithyderabad.edu.in>
2023-02-17 11:13:35 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask('__name__')
+lst = []
+l = [int(i) for i in range(len(lst))]
+d = {}
+for i in l:
+    if i not in d:
+        d[i] = lst[i]
+        print(d)
+
+
+@app.route('/', methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        task_content = request.form['task']
+        # task_content = request.form.get('task')
+        lst.append(task_content)
+        return redirect('/')
+    else:
+        return render_template("demo.html", lst=lst)
+
+
+@app.route('/delete/<string:content>')
+def remove(content):
+    lst.remove(content)
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    lst.clear()
+    app.run(debug=True, port=5003)
